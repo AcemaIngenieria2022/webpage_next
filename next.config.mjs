4fdbb8701bd1a2767c5276dacd2b6a4ac8b1304d
Santiago Montoya <santymm95@gmail.com>
2026-06-12 13:02:22 -0500

    Update Layoud

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,13 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'export', // <--- ESTO ES LO QUE FALTA
+  
+  // Opcional: Si tu sitio tiene problemas con rutas o imágenes, 
+  // a veces es necesario deshabilitar la optimización de imágenes:
+  images: {
+    unoptimized: true, 
+  },
 };
 
 export default nextConfig;

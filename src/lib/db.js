6722914c4968565f255741c1495e2ab6d0dcdd48
Santiago Montoya <santymm95@gmail.com>
2026-06-12 22:38:33 -0500

    Layoud Update

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,19 +1,15 @@
 import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-// Exportación nombrada
+// Ya no necesitamos importar ni configurar dotenv aquí
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || '3306', 10),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  
   waitForConnections: true,
   connectionLimit: 10,
   enableKeepAlive: true,
   keepAliveInitialDelay: 10000
 });
-
-// Opcional: Elimina la línea 'export default pool' si ya usas 'export const pool'

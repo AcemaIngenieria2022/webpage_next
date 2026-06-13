e5738c7e7ec7ef24725d7d1b7d601edac899f4db
Santiago Montoya <santymm95@gmail.com>
2026-06-12 22:29:00 -0500

    Layoud Update

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,20 +1,22 @@
 import mysql from 'mysql2/promise';
 
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1',
+  host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '3306', 10),
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'webpage_db',
+  user: process.env.DB_USER || 'u269079254_root',
+  password: process.env.DB_PASSWORD || 'Acema2022.s',
+  database: process.env.DB_NAME || 'u269079254_acema',
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+
   enableKeepAlive: true,
   keepAliveInitialDelay: 10000
 });
 
 pool.on('error', (err) => {
-  console.error('[XAMPP MySQL Pool Error]:', err.message);
+  console.error('[Hostinger MySQL Pool Error]:', err.message);
 });
 
 export default pool;
